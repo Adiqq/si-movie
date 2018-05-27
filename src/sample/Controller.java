fac84c704b7bc9dcd7c47e835706c51d5d8ab0b8
Adrian Wawrzak <wawrzak.adrian@ericsson.com>
2018-05-28 00:19:13 +0200

    init

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Label;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import java.util.Optional;
+
+public class Controller {
+    public Label result;
+    private KieServices ks;
+    private KieContainer kc;
+    public Controller() {
+        ks = KieServices.Factory.get();
+        kc = ks.getKieClasspathContainer();
+    }
+
+    public void start(ActionEvent actionEvent) {
+        KieSession ksession = kc.newKieSession("RomanticKS");
+        ksession.setGlobal( "result",
+                this.result );
+        ksession.fireAllRules();
+    }
+}
